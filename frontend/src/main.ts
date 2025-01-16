afde9a51606eee011ca9f667d496741a76c691dc
Orel Naranjo <orelnaranjo@gmail.com>
2025-01-16 16:42:25 -0300

    Add Font Awesome icons and update styles to use Poppins font

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,8 @@
 import './assets/main.scss'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faLock, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faCircleUser } from '@fortawesome/free-regular-svg-icons'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -6,8 +10,11 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+library.add(faCircleUser, faLock, faUser)
+
 const app = createApp(App)
 
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(createPinia())
 app.use(router)
 
